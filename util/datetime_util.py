b82ac3bfaa3872ea7343e6c6629040aa0d933d75
markli404 <yipingl4@illinois.edu>
2024-02-26 15:00:47 -0500

    change 'week' into 'week_index', allowing data grouped by week/month.

--- util/datetime_util.py
+++ util/datetime_util.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import datetime as dt
 from datetime import datetime
-
+import numpy as np
+pd.options.mode.chained_assignment = None  # default='warn'
 
 def datedelta_to_minutes(datedelta):
     days, seconds = datedelta.days, datedelta.seconds
@@ -10,32 +11,49 @@ def datedelta_to_minutes(datedelta):
 
 
 def ISO_string_to_datetime(s):
+    if s.endswith('Z'):
+        s = s[:-1]
     return datetime.fromisoformat(s).replace(tzinfo=None)
 
 
-def lable_week_and_month(df, date_keyword):
+def generate_index(some_list):
+    prev = some_list[0]
+    index = 1
+    for i in range(len(some_list)):
+        if prev != some_list[i]:
+            index += 1
+            prev = some_list[i]
+        some_list[i] = index
+    return some_list
+
+def lable_week_and_month(df, date_keyword, name_keyword='name'):
     df[date_keyword] = pd.to_datetime(df[date_keyword])
-    df['year'] = df[date_keyword].dt.year
-    df['month'] = df[date_keyword].dt.month
-    df['month_label'] = ''
-    df['week'] = df[date_keyword].dt.isocalendar().week
-    df['week_label'] = ''
-    df[date_keyword] = df[date_keyword].dt.date
+    res = []
+    for name in df[name_keyword].unique():
+        df_by_name = df[df[name_keyword] == name]
+        df_by_name = df_by_name.sort_values(by=date_keyword)
+        df_by_name[date_keyword] = pd.to_datetime(df_by_name[date_keyword])
 
-    current_year = dt.date.today().isocalendar().year
-    current_month = datetime.now().month
-    current_week = dt.date.today().isocalendar().week
+        month = df_by_name[date_keyword].dt.month.tolist()
+        week = df_by_name[date_keyword].dt.isocalendar().week.tolist()
 
-    df.loc[(df['year'] == current_year) & (df['month'] == current_month), 'month_label'] = '本月'
-    df.loc[(df['year'] == current_year) & (df['week'] == current_week), 'week_label'] = '本周'
+        # generate week index
+        df_by_name['week'] = generate_index(week)
+        df_by_name['month'] = generate_index(month)
 
-    last_week = dt.date.today() - dt.timedelta(days=7)
-    df.loc[(df['year'] == last_week.year) & (df['week'] == last_week.isocalendar().week), 'week_label'] = '上周'
+        df_by_name['week_label'] = ''
+        df_by_name['month_label'] = ''
 
-    if current_month == 1:
-        df.loc[(df['year'] == current_year - 1) & (df['month'] == 12), 'month'] = '上月'
-    else:
-        df.loc[(df['year'] == current_year) & (df['month'] == current_month - 1), 'month_label'] = '上月'
+        df_by_name[date_keyword] = df_by_name[date_keyword].dt.date
+        df_by_name.loc[df_by_name['month'] == max(df_by_name['month']), 'month_label'] = '本月'
+        df_by_name.loc[df_by_name['month'] == (max(df_by_name['month']) - 1), 'month_label'] = '上月'
+
+        df_by_name.loc[df_by_name['week'] == max(df_by_name['week']), 'week_label'] = '本周'
+        df_by_name.loc[df_by_name['week'] == (max(df_by_name['week']) - 1), 'week_label'] = '上周'
+
+        res.append(df_by_name)
+
+    df = pd.concat(res)
 
     return df
 
