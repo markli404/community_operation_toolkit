c54466f3c71b3bc8f4600c40c746e87a709105be
markli404 <yipingl4@illinois.edu>
2024-03-06 13:11:07 -0600

    implement through Hugging Face Hub API; Add hyperlink to model card

--- update_hugging_face_history.py
+++ update_hugging_face_history.py
@@ -11,65 +11,39 @@ with open('./config.yaml', 'r') as file:
 # Function to fetch all Hugging Face model cards that contain specific keywords
 def fetch_model_cards_by_name(name):
     print(f"Fetching model cards that contains keyword: {name}")
-    page = 0  # Initialize pagination
-    model_cards = []  # List to store found model cards
 
-    # Loop through paginated results until no more model cards are found
-    while True:
-        # Construct URL with pagination and search query
-        url = f'https://huggingface.co/models?p={page}&sort=trending&search={name}'
-        response = requests.get(url)  # Send HTTP GET request
+    model_cards = []# List to store found model cards
+    response = requests.get(
+        "https://huggingface.co/api/models",
+        params={"search": name, "full": "True"},
+        headers={}
+    ).json()
 
-        # Check if response is successful
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # Find all model card elements on the page
-            model_card = soup.find_all('a', class_='block p-2')
-            # Break loop if no model cards are found on the current page
-            if len(model_card) == 0:
-                break
-            else:
-                model_cards.extend(model_card)  # Add found model cards to the list
-                page += 1  # Increment page number to fetch next set of results
+    for model_card in response:
+        # Remove model cards that has 0 download
+        if model_card['downloads'] == 0:
+            continue
+
+        # Exclude model cards that does not contain the keyword in its name
+        if name not in model_card['id'].split('/')[1]:
+            continue
+
+        model_cards.append(model_card)
+
 
     return model_cards  # Return the list of found model cards
 
 
 # Function to format each model card into a structured data format
 def formatting_model_cards(model_card, base_model_name):
+    url = f"https://huggingface.co/{model_card['id']}"
+    name = model_card['id'].split('/')[1]
     # Extract model owner and name from the model card
-    model_card_df = {
-        'base_model': base_model_name,
-        'owner': (model_card.find('header').text.strip().split('/'))[0],
-        'name': (model_card.find('header').text.strip().split('/'))[1]
-    }
-
-    # Exclude model cards that do not include the base model name in their name
-    if base_model_name not in model_card_df['name']:
-        return None
-    # Convert updated time to ISO datetime format
-    model_card_df['updated_time'] = ISO_string_to_datetime(model_card.find('time')['datetime'])
-
-    # Extract download count, converting 'k' to thousands where applicable
-    try:
-        downloads = model_card.find('svg', class_='flex-none w-3 text-gray-400 mr-0.5').next_sibling.strip()
-        if 'k' in downloads:
-            downloads = float(downloads[:-1]) * 1000
-        downloads = int(downloads)
-    except:
-        downloads = 0  # Default to 0 if not found or on error
-
-    model_card_df['downloads'] = downloads  # Add downloads to the data
-
-    # Extract likes, converting 'k' to thousands where applicable
-    try:
-        likes = model_card.find('svg', class_='flex-none w-3 text-gray-400 mr-1').next_sibling.strip()
-        if 'k' in likes:
-            likes = float(likes[:-1]) * 1000
-    except:
-        likes = 0  # Default to 0 if not found or on error
-
-    model_card_df['likes'] = likes  # Add likes to the data
+    model_card_df = {'base_model': base_model_name,
+                     'owner': model_card['id'].split('/')[0],
+                     'name': f'=HYPERLINK("{url}", "{name}")',
+                     'downloads': model_card['downloads'],
+                     'create_at': ISO_string_to_datetime(model_card['createdAt'])}
 
     return model_card_df  # Return the formatted model card data
 
@@ -90,7 +64,7 @@ def get_all_hugging_face_history():
 
     # Label each model card with its week and month of update
     all_model_card_df = pd.DataFrame(all_model_card_df)
-    all_model_card_df = lable_week_and_month(all_model_card_df, 'updated_time', 'base_model')
+    all_model_card_df = lable_week_and_month(all_model_card_df, 'create_at', 'base_model')
 
     return all_model_card_df
 
@@ -117,3 +91,4 @@ def update_hugging_face_model_card_history():
 # Main execution block
 if __name__ == "__main__":
     update_hugging_face_model_card_history()  # Update the Hugging Face model
+
